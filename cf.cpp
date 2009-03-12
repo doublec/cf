0a17ee796dae321a4094abfad0c372ae20f1b7d9
Chris Double <chris@bethia.(none)>
2009-03-12 15:10:15 +1300

    Simple numbers

--- cf.cpp
+++ cf.cpp
@@ -1,15 +1,32 @@
+#include <cassert>
 #include <iostream>
+#include <sstream>
 #include <map>
 #include <vector>
 #include <deque>
 #include <string>
+
+class XY;
+class XYObject;
 
 using namespace std;
 
 class XYObject
 {
   public:
-    virtual string toString() { return string(""); }
+    virtual void eval1(XY* xy) = 0;
+    virtual string toString() = 0;
+};
+
+class XYNumber : public XYObject
+{
+  private:
+    int mValue;
+
+  public:
+    XYNumber(int v = 0);
+    virtual string toString();
+    virtual void eval1(XY* xy);
 };
 
 class XY {
@@ -19,24 +36,66 @@ class XY {
     deque<XYObject*> mY;
 
   public:
-    void print() {
-      for(int i=0; i < mX.size(); ++i) {
-        cout << mX[i]->toString() << " ";
-      }
-
-      cout << "-> ";
-
-      for(int i=0; i < mY.size(); ++i) {
-        cout << mY[i]->toString() << " ";
-      }
-
-      cout << endl;
-    }
-
+    void pushX(XYObject* o);
+    void pushFrontY(XYObject* o);
+    void pushBackY(XYObject* o);
+    void print();
+    XY* eval1();
 };
+
+void XY::pushX(XYObject* o) {
+  mX.push_back(o);
+}
+
+void XY::pushFrontY(XYObject* o) {
+  mY.push_front(o);
+}
+
+void XY::pushBackY(XYObject* o) {
+  mY.push_back(o);
+}
+
+void XY::print() {
+  for(int i=0; i < mX.size(); ++i) {
+    cout << mX[i]->toString() << " ";
+  }
+
+  cout << "-> ";
+
+  for(int i=0; i < mY.size(); ++i) {
+    cout << mY[i]->toString() << " ";
+  }
+
+  cout << endl;
+}
+
+XY* XY::eval1() {
+  assert(mY.size() > 0);
+
+  XYObject* o = mY.front();
+  assert(o);
+
+  mY.pop_front();
+  o->eval1(this);
+  return this;
+}
+ 
+XYNumber::XYNumber(int v) : mValue(v) { }
+string XYNumber::toString() {
+  ostringstream s;
+  s << mValue;
+  return s.str();
+}
+
+void XYNumber::eval1(XY* xy) {
+  xy->pushX(this);
+}
+
 
 void testXY() {
   XY* xy = new XY();
+  xy->pushX(new XYNumber(42));
+  xy->pushFrontY(new XYNumber(84));
   xy->print();
   delete xy;
 }
